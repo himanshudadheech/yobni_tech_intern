627119eded6cfebcb42e5ec6f99a5df1c9aea40a
Himanshu Dadheech <68987615+himanshudadheech@users.noreply.github.com>
2022-04-23 10:01:37 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,5 @@
-import React from "react";
-
-import { BottomNavigation } from "@mui/material";
 import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 export default function Footer() {
   return (
